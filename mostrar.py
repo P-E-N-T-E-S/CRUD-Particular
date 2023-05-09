280c3dbac2ff84c9aefd3e4db5621b828300ae9d
ThomazSpoiler <thomazrodrigues480@gmail.com>
2023-05-09 19:39:57 -0300

    implementação da função mostrar por categorial sem a biblioteca csv

--- mostrar.py
+++ mostrar.py
@@ -1,17 +1,19 @@
-import csv
 import os
 os.system('cls')
 categoria = []
 
 with open('controle.csv', 'r', encoding='utf-8') as arquivo:
-    lista_csv = csv.reader(arquivo, delimiter= ',')
+    lista_csv = arquivo.readlines()
     escolha = input("Qual categoria você deseja vizualizar? ").lower()
-    print("Essa categoria não existe, digite uma categoria válida")
-    for l in lista_csv:
-        if l[0] == 'ordem':
-            print("ordem  nome  categoria  valor  data  hora")
-        for a in range(len(l)):
+    if escolha in ['casa','lazer','trabalho']:
+        for l in lista_csv:
+            l = l.split(',')
+            if l[0] == 'ordem':
+                print("ordem  nome  categoria  valor  data  hora")
+            for a in range(len(l)):
+                if l[2] == escolha:
+                    print(l[a], end='\t')
             if l[2] == escolha:
-                print(l[a], end='\t')
-        if l[2] == escolha:
-            print()
+                print()
+    else:
+        print("Essa categoria não existe, digite uma categoria válida")
