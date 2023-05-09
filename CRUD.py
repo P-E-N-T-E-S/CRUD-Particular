b1eb6cb591c6ce5dac029a33a8ef129f8a3758d0
Luiz-Edu0202 <Brayner.luizeduardo@gmail.com>
2023-05-09 11:25:52 -0300

    Adição do Gitignore e Script que cria arquivos para teste

--- CRUD.py
+++ CRUD.py
@@ -17,18 +17,22 @@ def adicionar(arq, ordem, data, hora):
             nome = input("Digite o nome da transação: ")
             arquivo.write(f"\n{ordem +1},{nome},{categoria},{valor},{data},{hora}")
 
+
 def atualizar():
     memoria_csv = []
+    
     with open('Controle.CSV', 'r') as arquivo:
         arquivo_csv = csv.reader(arquivo)
         for linha in arquivo_csv:
             memoria_csv.append(linha[0].split(','))
     print('qual transação deseja alterar? (por index)')
+    
     for c in memoria_csv:
         for a in range(len(c)):
             print(c[a].ljust(9), end=' ')
         print()
     escolha = input()
+    
     for c in range(len(memoria_csv)):
         if memoria_csv[c] == escolha:
             memoria_csv.pop(c)
@@ -36,6 +40,7 @@ def atualizar():
             novacat = input('digite a nova categoria: ')
             novovalor = input('digite o novo valor da transação ')
             memoria_csv.insert(c, [novonome, novacat, novovalor])
+   
     with open('Controle.CSV', 'w') as arquivo:
         writer = csv.writer(arquivo, delimiter= ',')
         for i in memoria_csv:
